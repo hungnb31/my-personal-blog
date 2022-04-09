cdef49cc0784fba4bd3f97305cfa88be0523ea5a
Hung Nguyen Ba <hungnb31@gmail.com>
2022-04-09 12:03:33 +0700

    feat: updated responsive for series

--- apps/blog/components/Series.tsx
+++ apps/blog/components/Series.tsx
@@ -10,34 +10,30 @@ const Series = () => {
       <div className={styles.image}>
         <Image alt="something" src={showcaseGridImg} />
       </div>
-      <div className={styles.content}>
-        <div className={styles.articles}>
-          <article className={styles.article}>
-            <h4 className={styles.title}>This is the React post</h4>
-            <p>
-              Get your fill of more CSS Grid items in this collection. There are
-              a total of 32 pens to date... well, 33, if you count this one too.
-              What can you expect in this collection? The hidden gems
-            </p>
-          </article>
-          <article className={styles.article}>
-            <h4 className={styles.title}>This is the React post</h4>
-            <p>
-              Get your fill of more CSS Grid items in this collection. There are
-              a total of 32 pens to date... well, 33, if you count this one too.
-              What can you expect in this collection? The hidden gems
-            </p>
-          </article>
-          <article className={styles.article}>
-            <h4 className={styles.title}>This is the React post</h4>
-            <p>
-              Get your fill of more CSS Grid items in this collection. There are
-              a total of 32 pens to date... well, 33, if you count this one too.
-              What can you expect in this collection? The hidden gems
-            </p>
-          </article>
-        </div>
-      </div>
+      <article className={styles.article}>
+        <h4 className={styles.title}>This is the React post</h4>
+        <p className={styles.text}>
+          Get your fill of more CSS Grid items in this collection. There are a
+          total of 32 pens to date... well, 33, if you count this one too. What
+          can you expect in this collection? The hidden gems
+        </p>
+      </article>
+      <article className={styles.article}>
+        <h4 className={styles.title}>This is the React post</h4>
+        <p className={styles.text}>
+          Get your fill of more CSS Grid items in this collection. There are a
+          total of 32 pens to date... well, 33, if you count this one too. What
+          can you expect in this collection? The hidden gems
+        </p>
+      </article>
+      <article className={styles.article}>
+        <h4 className={styles.title}>This is the React post</h4>
+        <p className={styles.text}>
+          Get your fill of more CSS Grid items in this collection. There are a
+          total of 32 pens to date... well, 33, if you count this one too. What
+          can you expect in this collection? The hidden gems
+        </p>
+      </article>
     </div>
   );
 };
