661627dfaf048a25f516205bcc498ed2e63d5f99
Hung Nguyen <hungnb31@gmail.com>
2022-04-09 23:04:38 +0700

    feat: added new post and also generate markdown lib

--- apps/blog/pages/articles/[slug].tsx
+++ apps/blog/pages/articles/[slug].tsx
@@ -1,13 +1,33 @@
 import { ParsedUrlQuery } from 'querystring';
+import fs from 'fs';
+import { join } from 'path';
+
+import Layout from '@components/Layout';
+import { GetStaticPaths } from 'next';
 
 /* eslint-disable-next-line */
-export interface ArticleProps extends ParsedUrlQuery {}
+export interface ArticleProps extends ParsedUrlQuery {
+  slug: string;
+}
+
+const POSTS_PATH = join(process.cwd(), '_articles');
+
+export const getStaticPaths: GetStaticPaths<ArticleProps> = async () => {
+  const paths = fs
+    .readdirSync(POSTS_PATH)
+    .map((path) => path.replace(/\.mdx?$/, ''))
+    .map((slug) => ({ params: { slug } }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
 export function Article(props: ArticleProps) {
   return (
-    <div className="">
+    <Layout pageTitle="article" metaContent="article">
       <h1>Welcome to Article!</h1>
-    </div>
+    </Layout>
   );
 }
 
